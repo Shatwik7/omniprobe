82472063662cdac5ae48e4723405fd636abb52c7
Shatwik Pandey <shatwikpandey9706@gmail.com>
2026-03-06 11:21:31 +0000

    Edit app.e2e-spec.ts

--- apps/worker-service/test/app.e2e-spec.ts
+++ apps/worker-service/test/app.e2e-spec.ts
@@ -5,6 +5,43 @@ import { describe, beforeEach, it, expect, jest } from '@jest/globals';
 
 jest.setTimeout(30000);
 
+jest.mock('kafkajs', () => {
+  const events = {
+    CONNECT: 'connect',
+    DISCONNECT: 'disconnect',
+  };
+  return {
+    Kafka: class {
+      producer() {
+        return {
+          connect: () => Promise.resolve(),
+          disconnect: () => Promise.resolve(),
+          send: () => Promise.resolve(),
+          on: () => {},
+          events,
+        };
+      }
+      consumer() {
+        return {
+          connect: () => Promise.resolve(),
+          disconnect: () => Promise.resolve(),
+          subscribe: () => Promise.resolve(),
+          run: () => Promise.resolve(),
+          on: () => {},
+          events,
+        };
+      }
+    },
+    logLevel: {
+      NOTHING: 0,
+      ERROR: 1,
+      WARN: 2,
+      INFO: 4,
+      DEBUG: 5,
+    },
+  };
+});
+
 describe('WorkerServiceController (e2e)', () => {
   let app: INestApplication;
 
